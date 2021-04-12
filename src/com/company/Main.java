d7466ca1305ee6fc0c383d93b3c2c2df2d530c50
Egor Chitorkin <egor.chitorckin@yandex.ru>
2021-04-12 18:02:13 +0400

    Матрицы и системы счисления

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,18 +1,32 @@
 package com.company;
 
 import Operations.Integral;
+import Operations.Systems;
 import functions.Function;
 import functions.Graph;
+import functions.Matrix;
 import functions.basic.*;
 import functions.meta.*;
 import functions.parsers.FunctionParser;
 import functions.parsers.GraphParser;
+import functions.parsers.MatrixParser;
 
 public class Main {
 
     public static void main(String[] args) {
-        GraphParser graphParser = new GraphParser("[0,3,5,0,4,0,0;0,0,0,8,0,9,15;0,0,0,4,6,0,0;0,0,0,0,0,7,0;0,0,0,8,0,3,11;0,0,0,0,0,0,8;0,0,0,0,0,0,0]");
-        Graph graph = graphParser.parseGraph();
-        System.out.println(graph.Dijkstra(1,7));
+        MatrixParser matrixParser = new MatrixParser("[1,2,3;3,2, 1; 4,5,7]");
+        Matrix matrix = matrixParser.parseMatrix();
+        System.out.println(matrix);
+        Matrix[] lu = matrix.LU_decomposition();
+        System.out.println("L\n");
+        System.out.println(lu[0]);
+        System.out.println("U\n");
+        System.out.println(lu[1]);
+        System.out.println(Systems.from_p_to_q("AAABF56", 16, 10));
+        Function f = new Comp(new Sin(), new Cos());
+        System.out.println();
+        System.out.println(f);
+        System.out.println(f.derivative());
+
     }
 }
